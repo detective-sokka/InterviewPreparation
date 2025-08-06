3ba8c673bcfe6a9aba3f282c53da0882ab525e0b
detective-sokka <saidutt1997@gmail.com>
2025-08-06 19:43:35 -0400

    Add capacity member in Vector class and add vector to main.

--- CppConcepts/Vector/Vector.cpp
+++ CppConcepts/Vector/Vector.cpp
@@ -5,11 +5,13 @@ template <typename T>
 class Vector {
 public: 
     Vector() : m_ptr{new T[2]},
-               m_size{2} {                
+               m_size{2},
+               m_capacity{2}{
     }
 
     Vector(std::initializer_list<T> init) : m_ptr{new T[init.size()]},
-                                            m_size{init.size()} {        
+                                            m_size{init.size()},
+                                            m_capacity{m_size} {
         size_t index=0;
 
         for (auto& value : init) 
@@ -17,14 +19,16 @@ public:
     }
  
     Vector(const Vector<T>& obj) : m_ptr{new T[obj.size()]}, 
-                                   m_size{obj.size()} {           
+                                   m_size{obj.size()},
+                                   m_capacity{obj.size()} {
 
         for (size_t i=0; i < m_size; i++)
             m_ptr[i] = obj.m_ptr[i];
     }
 
     Vector(Vector&& obj) : m_ptr{obj.m_ptr}, 
-                           m_size{obj.size()} {        
+                           m_size{obj.size()},
+                           m_capacity{obj.size()} {
         obj.m_ptr = nullptr;
         obj.m_size = 0;        
     }    
@@ -75,6 +79,16 @@ public:
             std::cout << m_ptr[i] << ", ";
     }
 
+    T& operator[] (const int index) {
+        return m_ptr[index];
+    }
+
+    void pushBack(const T &value) {
+        if (m_size == m_capacity)
+            m_capacity *= 2;
+
+    }
+
     ~Vector() {
         delete[] m_ptr;
         m_size = 0;
@@ -83,6 +97,7 @@ public:
 private: 
         T* m_ptr;
         size_t m_size;
+        size_t m_capacity;
 };
 
 void test_custom_vector() {
@@ -95,4 +110,12 @@ void test_custom_vector() {
     a = c;
     a.display();
     b = std::move(c);
+    b[1] = 5;
+    b.display();
+
+    Vector<std::string> e = {"abc", "def"};
+    e.pushBack("ghi");
+    e.pushBack("jkl");
+    e.pushBack("mno");
+    e.display();
 }
--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // #include "CppConcepts/SharedPtr/SharedPtr.cpp"
 #include "CppConcepts/String/String.cpp"
 // #include "CppConcepts/UniquePtr/UniquePtr.cpp"
-// #include "CppConcepts/Vector/Vector.cpp"
+#include "CppConcepts/Vector/Vector.cpp"
 
 int main()
 {
@@ -15,8 +15,8 @@ int main()
     std::cout << "\n##### Test Custom String Class ##### ";
     // test_custom_string();
 
-    // std::cout << "\n##### Test Custom Vector Class ##### ";
-    // test_custom_vector();
+    std::cout << "\n##### Test Custom Vector Class ##### ";
+    test_custom_vector();
 
     return 0;
 }
